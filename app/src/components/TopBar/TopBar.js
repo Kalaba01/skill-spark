e5653fd6644c31f931ff807c9aa4b3882b6659a1
Kalaba <edinkalaba45@gmail.com>
2025-02-05 23:49:27 +0100

    Azurirana TopBar komponenta

--- app/src/components/TopBar/TopBar.js
+++ app/src/components/TopBar/TopBar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaUserCircle, FaBell, FaComments } from "react-icons/fa";
 import { Language, Theme, HamburgerMenu, Logout } from "../index";
+import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import "./TopBar.scss";
 
 function TopBar({ openLogin, variant = "default" }) {
+  const navigate = useNavigate();
   const accessToken = localStorage.getItem("access_token");
   let userRole = null;
 
@@ -21,7 +23,10 @@ function TopBar({ openLogin, variant = "default" }) {
     <header className="top-bar">
       {(variant !== "unauthorized" && variant !== "notfound") && userRole && <HamburgerMenu userRole={userRole} />}
 
-      <div className="logo">SkillSpark</div>
+      <div className="logo-container">
+        <img src="/logo.png" alt="SkillSpark Logo" className="logo-img" onClick={() => navigate("/")} />
+        <span className="logo-text">SkillSpark</span>
+      </div>
 
       <div className="top-bar-icons">
         {(variant === "unauthorized" || variant === "notfound") ? (
