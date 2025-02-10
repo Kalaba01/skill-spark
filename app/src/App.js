da0b2624f098297af22f27d7086465939413d141
Kalaba <edinkalaba45@gmail.com>
2025-02-10 17:41:25 +0100

    Dodat global styling za scroll

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ProtectedRoute, RedirectHome } from './middleware';
 import { GoTop, Footer, ToastNotification, Admin, Company, Employee, Unauthorized, NotFound, EmployeeManagement, UserManagement, ResetPassword, Quizzes } from './components';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
