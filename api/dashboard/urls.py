cdbe0d7595e0888a3aea9b8d83bf3ef71c517f01
Kalaba <edinkalaba45@gmail.com>
2025-02-21 18:56:00 +0100

    Dodat backend za employee statistiku

--- api/dashboard/urls.py
+++ api/dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AdminDashboardStatsView, CompanyDashboardStatsView
+from .views import AdminDashboardStatsView, CompanyDashboardStatsView, EmployeeDashboardStatsView
 
 urlpatterns = [
     path("admin-dashboard/", AdminDashboardStatsView.as_view(), name="admin-dashboard-stats"),
-    path("company-dashboard/", CompanyDashboardStatsView.as_view(), name="company-dashboard-stats")
+    path("company-dashboard/", CompanyDashboardStatsView.as_view(), name="company-dashboard-stats"),
+    path("employee-dashboard/", EmployeeDashboardStatsView.as_view(), name="employee-dashboard-stats")
 ]
--- api/dashboard/views.py
+++ api/dashboard/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from authentication.models import User, Company, Employee
-from quizzes.models import Quiz
+from quizzes.models import Quiz, PassedQuizzes
 
 class AdminDashboardStatsView(APIView):
     permission_classes = [IsAuthenticated]
@@ -33,6 +33,38 @@ class CompanyDashboardStatsView(APIView):
         stats = {
             "employees": Employee.objects.filter(company=company).count(),
             "quizzes": Quiz.objects.filter(company=company).count(),
+        }
+
+        return Response(stats)
+
+class EmployeeDashboardStatsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if request.user.role != User.EMPLOYEE:
+            raise PermissionDenied("Unauthorized access")
+
+        employee = request.user.employee_profile
+        passed_quizzes = PassedQuizzes.objects.filter(employee=employee).order_by("-passed_date")
+
+        last_quiz = passed_quizzes.first()
+        last_quiz_data = {
+            "title": last_quiz.quiz.title if last_quiz else None,
+            "date": last_quiz.passed_date.strftime("%Y-%m-%d") if last_quiz else None
+        }
+
+        all_quizzes = Quiz.objects.filter(company=employee.company)
+        passed_quiz_ids = passed_quizzes.values_list("quiz_id", flat=True)
+        recommended_quiz = all_quizzes.exclude(id__in=passed_quiz_ids).first()
+
+        recommended_quiz_data = {
+            "title": recommended_quiz.title if recommended_quiz else None
+        }
+
+        stats = {
+            "passed_quizzes": passed_quizzes.count(),
+            "last_quiz": last_quiz_data,
+            "recommended_quiz": recommended_quiz_data
         }
 
         return Response(stats)
