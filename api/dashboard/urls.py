10113de2358305d3531770f3cc762f355a82ec2a
Kalaba <edinkalaba45@gmail.com>
2025-02-21 01:17:46 +0100

    Dodat backend za company statistiku

--- api/dashboard/urls.py
+++ api/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AdminDashboardStatsView
+from .views import AdminDashboardStatsView, CompanyDashboardStatsView
 
 urlpatterns = [
-    path("admin-dashboard/", AdminDashboardStatsView.as_view(), name="admin-dashboard-stats")
+    path("admin-dashboard/", AdminDashboardStatsView.as_view(), name="admin-dashboard-stats"),
+    path("company-dashboard/", CompanyDashboardStatsView.as_view(), name="company-dashboard-stats")
 ]
--- api/dashboard/views.py
+++ api/dashboard/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from authentication.models import User
+from rest_framework.exceptions import PermissionDenied
+from authentication.models import User, Company, Employee
 from quizzes.models import Quiz
 
 class AdminDashboardStatsView(APIView):
@@ -9,7 +10,7 @@ class AdminDashboardStatsView(APIView):
 
     def get(self, request):
         if request.user.role != User.ADMIN:
-            return Response({"error": "Unauthorized"}, status=403)
+            raise PermissionDenied("Unauthorized access")
 
         stats = {
             "admins": User.objects.filter(role=User.ADMIN).count(),
@@ -17,5 +18,21 @@ class AdminDashboardStatsView(APIView):
             "employees": User.objects.filter(role=User.EMPLOYEE).count(),
             "quizzes": Quiz.objects.count(),
         }
-        
+
+        return Response(stats)
+
+class CompanyDashboardStatsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if request.user.role != User.COMPANY:
+            raise PermissionDenied("Unauthorized access")
+
+        company = request.user.company_profile
+
+        stats = {
+            "employees": Employee.objects.filter(company=company).count(),
+            "quizzes": Quiz.objects.filter(company=company).count(),
+        }
+
         return Response(stats)
